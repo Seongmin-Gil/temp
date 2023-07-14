6308719e6fd736ab28b88428c82e6166ea827aeb
길길성민 <ksm65501@gmail.com>
2023-07-14 16:28:13 +0900

    feat: 1초당 Mock Data 업로드

--- uploader.js
+++ uploader.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const { DataSource } = require("typeorm");
 require("dotenv").config();
 
+//DB 주소
 const appData = new DataSource({
   type: process.env.DB_TYPE,
   host: process.env.DB_HOST,
@@ -11,6 +12,7 @@ const appData = new DataSource({
   database: process.env.DB_DATABASE,
 });
 
+//MockData 전처리
 const csv = fs.readFileSync("../mockData.csv", "utf-8").trim();
 const temps = csv.split("\n");
 const rowDatas = temps.map((temp) => temp.split(","));
@@ -18,33 +20,32 @@ const header = rowDatas[0];
 const rows = rowDatas.splice(1);
 rows.map((row) => row.unshift("1"));
 
+let index = 0;
+
+//DB 연결
 appData
   .initialize()
   .then(() => {
     console.log("DataSource has been initialized!!");
-    while (index < rows.length) {
-      setTimeout(insertData, 1000);
-    }
+    //1초 간격으로 MockData Upload
+    setInterval(insertData, 1000);
   })
   .catch((err) => {
     console.error("Error during DataSource intitialization", err);
     appData.destroy();
   });
 
-// appData.query(
-//   `INSER INTO data (WellId, Flow, Static, Diff, Casing, Temp, Roads)
-//   VALUE ?;`
-// )
-
+//Mock Data UpLoad 합수
 const insertData = () => {
   if (index < rows.length) {
     appData.query(
       `INSERT INTO data (WellId, Flow, Static, Diff, Casing, Temp, Roads)
-    VALUES (?);`,
+      VALUES (?);`,
       [rows[index]]
     );
     index++;
   } else {
+    index = 0;
     return;
   }
 };
