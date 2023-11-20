2ee0635eb017cdd03d265756be4959b013886484
Logan <logan.gil@uitsolutions.com>
2023-11-20 15:50:28 +0900

    fix : change data format

--- uploader.js
+++ uploader.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
 const { DataSource } = require("typeorm");
+const xlsx = require("xlsx");
+
 require("dotenv").config();
+
+class WellData {
+
+}
+
+const dataArray = [];
+const wellArray = [];
+
+const dir = '../test';
 
 //DB 주소
 const appData = new DataSource({
@@ -12,13 +23,39 @@ const appData = new DataSource({
   database: process.env.DB_DATABASE,
 });
 
-//MockData 전처리
-const csv = fs.readFileSync("../mockData.csv", "utf-8").trim();
-const temps = csv.split("\n");
-const rowDatas = temps.map((temp) => temp.split(","));
-const header = rowDatas[0];
-const rows = rowDatas.splice(1);
-rows.map((row) => row.unshift("1"));
+const nullDataChange = (datas) => {
+  const index = datas.indexOf("");
+  if(index === -1){
+    return;
+  } else {
+    datas[index] = null;
+  }
+}
+
+const files = fs.readdirSync(dir);
+//데이터 전처리
+for (let i = 0; i < files.length; i++) {
+  const newData = new WellData();
+  const well = files[i].slice(0, -30);
+  newData.well = well;
+  console.log(well);
+  const excelFile = xlsx.readFile(dir+"/"+files[i], { type: 'array'});
+  const sheetName = excelFile.SheetNames[0];
+  const sheet = excelFile.Sheets[sheetName];
+  const data = xlsx.utils.sheet_to_csv(sheet);
+  const temps = data.split("\n");
+  const rowDatas = temps.map((temp) => temp.split(","));
+  const header = rowDatas[0];
+  const rows = rowDatas.splice(1);
+  rows.map((row) => {
+    nullDataChange(row);
+    row.unshift(i+1);
+  });
+  newData.data = rows;
+  dataArray.push(newData);
+  wellArray.push(well);
+}
+
 
 let index = 0;
 let time = new Array();
@@ -38,26 +75,50 @@ const uploader = () => {
     });
 };
 
-//Mock Data UpLoad 합수
+//Data UpLoad 합수
 const insertData = () => {
   const startTime = performance.now();
-  if (index < rows.length) {
-    appData.query(
-      `INSERT INTO data (WellId, Flow, Static, Diff, Casing, Temp, Roads)
-      VALUES (?);`,
-      [rows[index]]
-    );
+  if (index < dataArray[0].data.length) {
+    for(let i = 0; i < dataArray.length; i++) {
+      appData.query(
+        `INSERT INTO data (WellId, CurrentTime, GasFlowRate, TodayFlow, FlowTimeToday, StaticPressure, DiffPressure, Temperature, CondenstateToday, ESDZSO, HighSepLevel, OrificePlate, Voltage)
+        VALUES (?);`,
+        [dataArray[i].data[index]]
+      );
+      //처리 속도 측정
+      const endTime = performance.now();
+      const deltaTime = endTime - startTime;
+      time.push(deltaTime);
+    }
     index++;
     console.log(`${index}번째 데이터 업로드 완료`);
   } else {
     index = 0;
     time = new Array();
   }
-  //처리 속도 측정
-  const endTime = performance.now();
-  const deltaTime = endTime - startTime;
-  time.push(deltaTime);
 };
+
+// const insertData = () => {
+//   const startTime = performance.now();
+//   if (index < rows.length) {
+//     appData.query(
+//       `INSERT INTO data (WellId, Flow, Static, Diff, Casing, Temp, Roads)
+//       VALUES (?);`,
+//       // INSERT INTO data (WellId, GasFlowRate, TodayFlow, FlowTimeToday, StaticPressure, DiffPressure, Temperature, CondenstateToday, ESDZSO, HighSepLevel, OrificePlate, Voltage)
+//       // VALUES (?);,
+//       [rows[index]]
+//     );
+//     index++;
+//     console.log(`${index}번째 데이터 업로드 완료`);
+//   } else {
+//     index = 0;
+//     time = new Array();
+//   }
+//   //처리 속도 측정
+//   const endTime = performance.now();
+//   const deltaTime = endTime - startTime;
+//   time.push(deltaTime);
+// };
 
 //처리 속도 전달
 const getTimeArray = () => {
